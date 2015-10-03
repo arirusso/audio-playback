6d2f434c4461150a3abe05b38424e06cd65bac12
Ari Russo <ari.russo@gmail.com>
2015-10-03 15:54:26 -0400

    mock classes

--- test/helper.rb
+++ test/helper.rb
@@ -4,3 +4,26 @@ require "minitest/autorun"
 require "mocha/test_unit"
 require "shoulda-context"
 require "audio-playback"
+
+class MockOutput
+
+  attr_reader :num_channels
+
+  def initialize(id, options = {})
+    @id = id
+    @num_channels = options[:num_channels] || 2
+  end
+
+  def latency
+    1
+  end
+
+end
+
+class MockStream
+
+  def initialize(output)
+    @output = output
+  end
+
+end
