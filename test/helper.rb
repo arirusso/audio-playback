6cf0af53e83abc42676014d486a892d93c3eda3b
Ari Russo <ari.russo@gmail.com>
2015-10-06 20:38:57 -0400

    stub out portaudio methods

--- test/helper.rb
+++ test/helper.rb
@@ -27,3 +27,28 @@ class MockStream
   end
 
 end
+
+module TestHelper
+
+  DEFAULT_OUTPUT_ID = 0
+  OUTPUT_INFO = [
+    {
+      :defaultHighOutputLatency => 2.3,
+      :maxOutputChannels => 1,
+      :name => "Test Output 1"
+    },
+    {
+      :defaultHighOutputLatency => 3.2,
+      :maxOutputChannels => 2,
+      :name => "Test Output 2"
+    }
+  ]
+
+end
+
+FFI::PortAudio::API.stubs(:Pa_GetDeviceCount).returns(2)
+FFI::PortAudio::API.expects(:Pa_GetDeviceInfo).with(0).at_least_once.returns(TestHelper::OUTPUT_INFO[0])
+FFI::PortAudio::API.expects(:Pa_GetDeviceInfo).with(1).at_least_once.returns(TestHelper::OUTPUT_INFO[1])
+FFI::PortAudio::API.stubs(:Pa_GetDefaultOutputDevice).returns(TestHelper::DEFAULT_OUTPUT_ID)
+FFI::PortAudio::API.stubs(:Pa_Initialize).returns(true)
+FFI::PortAudio::API.stubs(:Pa_Terminate).returns(true)
