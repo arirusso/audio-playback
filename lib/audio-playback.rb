202dd65bbd6f80b8145f557423ce75f3dad48560
Ari Russo <ari.russo@gmail.com>
2015-10-11 22:39:59 -0400

    comments

--- lib/audio-playback.rb
+++ lib/audio-playback.rb
@@ -23,16 +23,27 @@ require "audio-playback/playback"
 require "audio-playback/sound"
 require "audio-playback/stream"
 
+# Play audio files
 module AudioPlayback
 
   VERSION = "0.0.1"
 
-  def self.play(filename, options = {})
-    sound = Sound.load(filename, options)
+  # Convenience method to play an audio file
+  # @param [File, String] file_path
+  # @param [Hash] options
+  # @option options [Fixnum] :buffer_size Buffer size in bytes.  Defaults to 4096
+  # @option options [Array<Fixnum>, Fixnum] :channels Output audio to the given channel(s).  Eg `:channels => [0,1]` will direct the audio to channels 0 and 1. Defaults to use all available channels
+  # @option options [Float] :latency Latency in seconds.  Defaults to use the default latency for the selected output device
+  # @option options [IO] :logger Logger object
+  # @option options [Fixnum, String] :output_device Output device id or name
+  def self.play(file_path, options = {})
+    sound = Sound.load(file_path, options)
     output = Output.by_name(options[:output_device]) || Output.by_id(options[:output_device]) || Device.default_output
     Playback.play(sound, output, options)
   end
 
+  # Ensure that the audio system is initialized
+  # @return [Boolean]
   def self.ensure_initialized
     @initialized ||= FFI::PortAudio::API.Pa_Initialize
   end
