a2ad71903c3c9ca70418b5ec9f02ccbf41333ba3
Ari Russo <ari.russo@gmail.com>
2015-10-16 19:38:38 -0400

    comments

--- lib/audio-playback/playback/stream_data.rb
+++ lib/audio-playback/playback/stream_data.rb
@@ -2,6 +2,7 @@ module AudioPlayback
 
   module Playback
 
+    # Playback data for the Device::Stream
     class StreamData
 
       # A C pointer version of the audio data
@@ -10,12 +11,14 @@ module AudioPlayback
         stream_data.to_pointer
       end
 
+      # @param [Playback::Action] playback
       def initialize(playback)
         @playback = playback
         populate
       end
 
       # A C pointer version of the audio data
+      # @return [FFI::Pointer]
       def to_pointer
         pointer = FFI::LibC.malloc(@playback.data_size)
         pointer.write_array_of_float(@data.flatten)
@@ -24,12 +27,16 @@ module AudioPlayback
 
       private
 
+      # Populate the playback stream data
+      # @return [FrameSet]
       def populate
         @data = FrameSet.new(@playback)
         add_metadata
         @data
       end
 
+      # Add playback metadata to the stream data
+      # @return [FrameSet]
       def add_metadata
         @data.unshift(0.0) # 3. is_eof
         @data.unshift(0.0) # 2. counter
@@ -39,7 +46,7 @@ module AudioPlayback
       end
 
     end
-    
+
   end
 
 end
