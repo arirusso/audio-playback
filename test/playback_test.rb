ab8173519182bab43c12d3cb3c5c40b4c40487ff
Ari Russo <ari.russo@gmail.com>
2015-10-15 20:31:05 -0400

    not needed

--- test/playback_test.rb
+++ test/playback_test.rb
@@ -106,126 +106,108 @@ class AudioPlayback::PlaybackTest < Minitest::Test
 
     end
 
-    context "#frames" do
+    context "Frames" do
 
-      setup do
-        @playback = AudioPlayback::Playback.new(@sound, @output)
-      end
+      context "#ensure_structure" do
 
-      should "have the correct number of frames" do
-        assert_equal @sound.size + AudioPlayback::Playback::METADATA.size, @playback.frames.size
-      end
+        context "sound has same number of channels as output" do
 
-      should "be the correct format" do
-        assert @playback.frames.all? do |frame|
-          if frame.kind_of?(Array)
-            frame.all? { |num| num.is_a?(Float) }
-          else
-            frame.is_a?(Float)
-          end
-        end
-      end
+          context "mono file, mono output" do
 
-    end
+            setup do
+              @path = "test/media/1-mono-44100.wav"
+              @file = AudioPlayback::File.new(@path)
+              @sound = AudioPlayback::Sound.new(@file)
+              @output = MockOutput.new(1, :num_channels => 1)
+              @playback = AudioPlayback::Playback.new(@sound, @output, :stream => MockStream.new(@output))
+              @data = @sound.data
+              @frames = AudioPlayback::Playback::Frames.ensure_structure(@playback).slice(AudioPlayback::Playback::METADATA.size..-1)
+            end
 
-    context "#ensure_structure" do
+            should "have two channels of valid data" do
+              refute_nil @frames
+              assert @frames.kind_of?(Array)
+              refute_empty @frames
+              assert @frames.all? { |frame_channels| frame_channels.count == @output.num_channels }
+              assert @frames.all? { |frame_channels| frame_channels.count == @sound.num_channels }
+              assert @frames.all? { |frame_channels| frame_channels.all? { |frame| frame.kind_of?(Float) } }
+            end
 
-      context "sound has same number of channels as output" do
-
-        context "mono file, mono output" do
-
-          setup do
-            @path = "test/media/1-mono-44100.wav"
-            @file = AudioPlayback::File.new(@path)
-            @sound = AudioPlayback::Sound.new(@file)
-            @output = MockOutput.new(1, :num_channels => 1)
-            @playback = AudioPlayback::Playback.new(@sound, @output, :stream => MockStream.new(@output))
-            @data = @sound.data
-            @frames = @playback.frames.slice(AudioPlayback::Playback::METADATA.size..-1)
           end
 
-          should "have two channels of valid data" do
-            refute_nil @frames
-            assert @frames.kind_of?(Array)
-            refute_empty @frames
-            assert @frames.all? { |frame_channels| frame_channels.count == @output.num_channels }
-            assert @frames.all? { |frame_channels| frame_channels.count == @sound.num_channels }
-            assert @frames.all? { |frame_channels| frame_channels.all? { |frame| frame.kind_of?(Float) } }
-          end
+          context "stereo file, stereo output" do
 
-        end
+            setup do
+              @path = "test/media/1-stereo-44100.wav"
+              @file = AudioPlayback::File.new(@path)
+              @sound = AudioPlayback::Sound.new(@file)
+              @output = MockOutput.new(1, :num_channels => 2)
+              @playback = AudioPlayback::Playback.new(@sound, @output, :stream => MockStream.new(@output))
+              @data = @sound.data
+              @frames = AudioPlayback::Playback::Frames.ensure_structure(@playback).slice(AudioPlayback::Playback::METADATA.size..-1)
+            end
 
-        context "stereo file, stereo output" do
+            should "have one channel of valid data" do
+              refute_nil @frames
+              assert @frames.kind_of?(Array)
+              refute_empty @frames
+              assert @frames.all? { |frame_channels| frame_channels.kind_of?(Array) }
+              assert @frames.all? { |frame_channels| frame_channels.count == @output.num_channels }
+              assert @frames.all? { |frame_channels| frame_channels.count == @sound.num_channels }
+              assert @frames.all? { |frame_channels| frame_channels.all? { |frame| frame.kind_of?(Float) } }
+            end
 
-          setup do
-            @path = "test/media/1-stereo-44100.wav"
-            @file = AudioPlayback::File.new(@path)
-            @sound = AudioPlayback::Sound.new(@file)
-            @output = MockOutput.new(1, :num_channels => 2)
-            @playback = AudioPlayback::Playback.new(@sound, @output, :stream => MockStream.new(@output))
-            @data = @sound.data
-            @frames = @playback.frames.slice(AudioPlayback::Playback::METADATA.size..-1)
-          end
-
-          should "have one channel of valid data" do
-            refute_nil @frames
-            assert @frames.kind_of?(Array)
-            refute_empty @frames
-            assert @frames.all? { |frame_channels| frame_channels.kind_of?(Array) }
-            assert @frames.all? { |frame_channels| frame_channels.count == @output.num_channels }
-            assert @frames.all? { |frame_channels| frame_channels.count == @sound.num_channels }
-            assert @frames.all? { |frame_channels| frame_channels.all? { |frame| frame.kind_of?(Float) } }
           end
 
         end
 
-      end
+        context "sound has different number of channels than output" do
 
-      context "sound has different number of channels than output" do
+          context "mono file, stereo output" do
 
-        context "mono file, stereo output" do
+            setup do
+              @path = "test/media/1-mono-44100.wav"
+              @file = AudioPlayback::File.new(@path)
+              @sound = AudioPlayback::Sound.new(@file)
+              @output = MockOutput.new(1, :num_channels => 2)
+              @playback = AudioPlayback::Playback.new(@sound, @output, :stream => MockStream.new(@output))
+              @data = @sound.data
+              @frames = AudioPlayback::Playback::Frames.ensure_structure(@playback).slice(AudioPlayback::Playback::METADATA.size..-1)
+            end
 
-          setup do
-            @path = "test/media/1-mono-44100.wav"
-            @file = AudioPlayback::File.new(@path)
-            @sound = AudioPlayback::Sound.new(@file)
-            @output = MockOutput.new(1, :num_channels => 2)
-            @playback = AudioPlayback::Playback.new(@sound, @output, :stream => MockStream.new(@output))
-            @data = @sound.data
-            @frames = @playback.frames.slice(AudioPlayback::Playback::METADATA.size..-1)
+            should "have two channels of valid data" do
+              refute_nil @frames
+              assert @frames.kind_of?(Array)
+              refute_empty @frames
+              assert @frames.all? { |frame_channels| frame_channels.count == @output.num_channels }
+              refute @frames.any? { |frame_channels| frame_channels.count == @sound.num_channels }
+              assert @frames.all? { |frame_channels| frame_channels.all? { |frame| frame.kind_of?(Float) } }
+            end
+
           end
 
-          should "have two channels of valid data" do
-            refute_nil @frames
-            assert @frames.kind_of?(Array)
-            refute_empty @frames
-            assert @frames.all? { |frame_channels| frame_channels.count == @output.num_channels }
-            refute @frames.any? { |frame_channels| frame_channels.count == @sound.num_channels }
-            assert @frames.all? { |frame_channels| frame_channels.all? { |frame| frame.kind_of?(Float) } }
-          end
+          context "stereo file, mono output" do
 
-        end
+            setup do
+              @path = "test/media/1-stereo-44100.wav"
+              @file = AudioPlayback::File.new(@path)
+              @sound = AudioPlayback::Sound.new(@file)
+              @output = MockOutput.new(1, :num_channels => 1)
+              @playback = AudioPlayback::Playback.new(@sound, @output, :stream => MockStream.new(@output))
+              @data = @sound.data
+              @frames = AudioPlayback::Playback::Frames.ensure_structure(@playback).slice(AudioPlayback::Playback::METADATA.size..-1)
+            end
 
-        context "stereo file, mono output" do
+            should "have one channel of valid data" do
+              refute_nil @frames
+              assert @frames.kind_of?(Array)
+              refute_empty @frames
+              assert @frames.all? { |frame_channels| frame_channels.kind_of?(Array) }
+              assert @frames.all? { |frame_channels| frame_channels.count == @output.num_channels }
+              refute @frames.any? { |frame_channels| frame_channels.count == @sound.num_channels }
+              assert @frames.all? { |frame_channels| frame_channels.all? { |frame| frame.kind_of?(Float) } }
+            end
 
-          setup do
-            @path = "test/media/1-stereo-44100.wav"
-            @file = AudioPlayback::File.new(@path)
-            @sound = AudioPlayback::Sound.new(@file)
-            @output = MockOutput.new(1, :num_channels => 1)
-            @playback = AudioPlayback::Playback.new(@sound, @output, :stream => MockStream.new(@output))
-            @data = @sound.data
-            @frames = @playback.frames.slice(AudioPlayback::Playback::METADATA.size..-1)
-          end
-
-          should "have one channel of valid data" do
-            refute_nil @frames
-            assert @frames.kind_of?(Array)
-            refute_empty @frames
-            assert @frames.all? { |frame_channels| frame_channels.kind_of?(Array) }
-            assert @frames.all? { |frame_channels| frame_channels.count == @output.num_channels }
-            refute @frames.any? { |frame_channels| frame_channels.count == @sound.num_channels }
-            assert @frames.all? { |frame_channels| frame_channels.all? { |frame| frame.kind_of?(Float) } }
           end
 
         end
