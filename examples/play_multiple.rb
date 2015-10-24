9879af4fce45e044e10ada94ba64763b830ebc8a
Ari Russo <ari.russo@gmail.com>
2015-10-23 20:53:13 -0400

    first pass at play multiple example

---
+++ examples/play_multiple.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+$:.unshift(File.join("..", "lib"))
+
+# Select one of the test files for playback
+
+require "audio-playback"
+
+MEDIA_DIRECTORY = "../test/media"
+
+# Select an output
+@output = AudioPlayback::Device::Output.gets
+
+# Find audio files
+audio_files = Dir.entries(MEDIA_DIRECTORY)
+audio_files.reject! { |file| file.match(/^\.{1,2}$/) }
+
+# Select two random files
+@files = audio_files.sample(2)
+
+@sounds = @files.map { |filename| AudioPlayback::Sound.load("#{MEDIA_DIRECTORY}/#{filename}") }
+
+# Play files
+@playbacks = @sounds.map do |sound|
+  AudioPlayback::Playback.new(sound, @output)
+end
+
+@playbacks.each(&:start)
+
+@playbacks.last.block
