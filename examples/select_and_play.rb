9e6f0c147c2139e25af33d626075d80d3e49676b
Ari Russo <ari.russo@gmail.com>
2015-10-24 11:49:04 -0400

    comments

--- examples/select_and_play.rb
+++ examples/select_and_play.rb
@@ -10,18 +10,18 @@ MEDIA_DIRECTORY = "../test/media"
 # Select an output
 @output = AudioPlayback::Device::Output.gets
 
-# find audio files
+# Find audio files
 audio_files = Dir.entries(MEDIA_DIRECTORY)
 audio_files.reject! { |file| file.match(/^\.{1,2}$/) }
 
 puts
 puts "Select an audio file..."
 
-# print list of files
+# Print list of files
 audio_files.each_with_index { |file, i| puts("#{i+1}. #{file}") }
 puts
 
-# prompt user to select a file
+# Prompt user to select a file
 filename = nil
 while filename.nil? do
   print("> ")
@@ -30,6 +30,6 @@ while filename.nil? do
   filename = audio_files[index]
 end
 
-# play file
+# Play file
 @playback = AudioPlayback.play("#{MEDIA_DIRECTORY}/#{filename}", :output_device => @output)
 @playback.block
